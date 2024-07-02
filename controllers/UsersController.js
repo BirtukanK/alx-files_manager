f655e5c2054404b0e46f0f6c91304c95aad6ca47
BirtukanK <birtukankuma1113@gmail.com>
2024-07-01 17:05:43 -0700

    create new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,36 @@
+import dbClient from '../utils/db';
+
+const crypto = require('crypto');
+
+class UsersController {
+  static async postNew(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email) {
+        return res.status(400).json({ error: 'Missing email' });
+      }
+
+      if (!password) {
+        return res.status(400).json({ error: 'Missing password' });
+      }
+
+      const existingUser = await dbClient.db.collection('users').findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+      const newUser = { email, password: hashedPassword };
+
+      const result = await dbClient.db.collection('users').insertOne(newUser);
+
+      return res.status(201).json({ id: result.insertedId, email });
+    } catch (error) {
+      console.error('Error creating user:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = UsersController;

20390f30edcd434332e953ededfb5369279483f7
BirtukanK <birtukankuma1113@gmail.com>
2024-07-01 18:19:36 -0700

    user authentication

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -31,6 +31,28 @@ class UsersController {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
+
+  static async getMe(req, res) {
+    try {
+      const token = req.headers['x-token'];
+      const key = `auth_${token}`;
+
+      const userId = await redisClient.get(key);
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const user = await dbClient.db.collection('users').findOne({ _id: new dbClient.ObjectId(userId) }, { projection: { email: 1 } });
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      return res.status(200).json({ id: user._id, email: user.email });
+    } catch (error) {
+      console.error('Error in getMe:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
 }
 
 module.exports = UsersController;
